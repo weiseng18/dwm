171b79d81388dc21ca0940c0bccb49a34cafc67b
weiseng18 <tws.hci@gmail.com>
2021-07-14 23:39:56 +0800

    change colors

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 7;       /* vert outer gap between windows
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "NotoSansDisplay Nerd Font:SemiBold:size=12" };
+static const char *fonts[]          = { "NotoSansDisplay Nerd Font:SemiBold:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +19,8 @@ static const char col_cyan[]        = "#0092cc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
+	[SchemeTitle] = { col_gray3, col_gray1, col_gray2 },
 };
 
 /* tagging */
