3f62067f56fa939c22458d54a5e6cbb5b6395392
weiseng18 <tws.hci@gmail.com>
2021-09-28 10:50:45 +0800

    rule for todoist

--- config.h
+++ config.h
@@ -37,7 +37,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,       NULL,       NULL,       0,            False,       -1 },
+	{ NULL,       NULL,       NULL,          0,          False,       -1 },
+	{ "Todoist",  NULL,       NULL,       1<<1,          False,        1 },
 };
 
 /* layout(s) */
